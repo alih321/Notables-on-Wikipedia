bbb54a243f92c26ec126e21fa2d42f939b14e2c9
LucAlexander <lucskyalex@gmail.com>
2022-02-15 02:46:10 -0500

    rewrote algorithm, made it faster, added colorful loading bar

--- pt1.py
+++ pt1.py
@@ -8,63 +8,6 @@ from urllib.request import urlopen;
 import csv
 import re
 
-def main():
-    depth = 2
-    #seed = "https://en.wikipedia.org/wiki/Luc_Bourdon"
-    seed = "https://en.wikipedia.org/wiki/Muhammad_Ali"
-    seedTup = tuple([seed, seed])
-    urls = [seedTup] # master list of urls
-    linkConnections = list() # final list of extracted links in tuple form (source, destination)
-    personInfo = dict() # final dictionary of person information to be exported to csv
-    nextDepth = list() # temporary url storage 
-    # Read through collected links as tuples containing (source, destination)
-    # this starts as (seed, seed)
-    for i, urlPair in enumerate(urls):
-        source = urlPair[0]
-        url = urlPair[1]
-        if depth > 0:
-            crawl(source, url, nextDepth, personInfo, linkConnections)
-        # if all urls at a depth have been crawled, append all data to master urls list
-        # clear temprorary url storage
-        if i == len(urls)-1:
-            print("layer " + str(3-depth) + " completed")
-            urls += nextDepth
-            nextDepth = list()
-            depth -= 1
-    linkConnections = linkConnections[1:]
-    print("Crawling finished")
-    print(linkConnections)
-    print(personInfo)
-    print("Writing to CSV")
-    with open("links.csv", "w+") as infilecsv:
-        writer = csv.writer(infilecsv)
-        writer.writerow(["from_url","to_url"])
-        for row in linkConnections:
-            parsed = list(row)
-            for i, item in enumerate(parsed):
-                parsed[i] = item[30:]
-            writer.writerow(parsed)
-    with open("notables.csv", "w+") as infilecsv:
-        writer = csv.writer(infilecsv)
-        writer.writerow(["url","name","born","died"])
-        for key in personInfo:
-            writer.writerow(tuple([key])+personInfo[key])
-    print("Writing to CSV finished")
-
-def crawl(source, url, nextDepth, personInfo, linkConnections):
-    soup = getSoupFromUrl(url)
-    parsedUrl = url[30:] # get name from wikipedia link
-    # we make sure not to download the url again if already parsed
-    if parsedUrl not in personInfo:
-        info = getPersonInformation(soup)
-        if info == False:
-            return
-        personInfo[parsedUrl] = info
-    # regardless of visited or not, store as tuple in final list
-    linkConnections.append(tuple([source, url]))
-    # add to temprary url storage medium
-    nextDepth += getAllLinksFromPage(url, soup)
-
 def getSoupFromUrl(url):
     response = urlopen(url)
     soup = BeautifulSoup(response, "lxml")
@@ -75,7 +18,7 @@ def getPersonInformation(soup):
     #Simple Information (Name, YoB, YoD)
     
     if not checkPageSignificance(soup):
-        print("NOT SIGNIFICANT!", soup.url)
+        #not significant
         return False;
     
     name = soup.find_all("h1")[0].text
@@ -102,29 +45,75 @@ def getPersonInformation(soup):
     else:
         return False
         
-    print(yob, "\n\n")
-    print(yod, "\n\n")
-    
     return (name, yob, yod)
-
 
 def checkPageSignificance(soup):
     return ((soup.find(class_="infobox-label", string="Born") is not None) and (soup.find(class_="infobox-label", string="Died") is not None))
-        
 
-def getAllLinksFromPage(source, soup):
-    #Get all links in Tuple
+def getAllLinksFromPage(soup):
+    # get all links in page
     allLinks = []
     bodyContent = soup.find("div", {"id": "bodyContent"})
     for tag in bodyContent.find_all("a"):
         if tag.has_attr("href"):
             pulledUrl = tag.get("href")
             if (pulledUrl[:6] == "/wiki/"):
-                pair = [source, "https://en.wikipedia.org" + pulledUrl]
-                allLinks.append(tuple(pair)) 
-    for links in allLinks:
-        print(source + "->" + str(links[1]))
+                pair = "https://en.wikipedia.org" + pulledUrl
+                allLinks.append(pair) 
     return allLinks;
+       
+def crawl(source, url, depth, startDepth, infoData, linkData):
+    if depth < 0:
+        return
+    soup = getSoupFromUrl(url)
+    parsedUrl = url[30:] # get name from wikipedia link
+    if parsedUrl not in infoData:
+        info = getPersonInformation(soup)
+        if info == False:
+            return
+        infoData[parsedUrl] = info
+        links = getAllLinksFromPage(soup)
+        for i, suburl in enumerate(links):
+            crawl(url, suburl, depth-1, startDepth, infoData, linkData)
+            if depth == startDepth:
+                print("\r\tPROGRESS [\033[1;31m", end="")
+                k = 0
+                while(k < ((i/len(links))*100)):
+                    print("#",end="")
+                    k+=1
+                while(k<100):
+                    print(" ",end="")
+                    k+=1
+                print("\033[0m]",(i*100)//len(links),"%",end="")
+    linkData.append(tuple([source, url]))
 
-if __name__ == "__main__":
-    main();
+def writeToCsv(infoData, linkData):
+    with open("links.csv", "w+") as infilecsv:
+        writer = csv.writer(infilecsv)
+        writer.writerow(["from_url","to_url"])
+        for row in linkData:
+            parsed = list(row)
+            for i, item in enumerate(parsed):
+                parsed[i] = item[30:]
+            writer.writerow(parsed)
+    with open("notables.csv", "w+") as infilecsv:
+        writer = csv.writer(infilecsv)
+        writer.writerow(["url","name","born","died"])
+        for key in infoData:
+            writer.writerow(tuple([key])+infoData[key])
+
+def main():
+    infoData = dict()
+    linkData = list()
+    seed = "https://en.wikipedia.org/wiki/Luc_Hoffmann"
+    startDepth = 2
+    depth = startDepth
+    print("\n\tCrawling ", seed, " at depth ", startDepth, "\n")
+    crawl(seed, seed, depth, startDepth, infoData, linkData)
+    writeToCsv(infoData, linkData)
+    print("\n\tDone")
+
+if __name__=="__main__":
+    main()
+
+
